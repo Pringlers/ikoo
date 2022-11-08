d472c4f636c328b366091fd1edb53964717e7869
Matees <mateespublicprofile@gmail.com>
2022-11-07 23:09:51 -0500

    added file running example

---
+++ examples/file.rs
@@ -0,0 +1,21 @@
+use ikoo::{lexer::Lexer, parser::Parser};
+use std::fs;
+
+fn main() {
+    let Some(input) = std::env::args().nth(1) else {
+        panic!("Please supply file to parse");
+    };
+
+    let input_file = fs::read_to_string(input)
+        .expect("Given file does not exist");
+
+    println!("Parsing file contents -> \n{}", input_file);
+
+    let lexer: Lexer = Lexer::new(&input_file);
+    let mut parser = Parser::new(&input_file, lexer);
+
+    let module = parser.parse_module();
+    for statement in module.stmts {
+        println!("{statement:#?}");
+    }
+}
